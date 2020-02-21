d22088ccf8df24df4f56ee8e30823a201ab6736f
MARIANA RAQUEL DE MORAIS <morais.mariana@academico.ifrn.edu.br>
2020-02-21 15:42:40 -0300

    Exercício 11

---
+++ Lista 2/Exercício10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma data no formato dd/mm/aaaa");
+            string[] data = Console.ReadLine().Split('/');
+            int dia = int.Parse(data[0]);
+            int mes = int.Parse(data[1]);
+            int ano = int.Parse(data[2]);
+            if (ano >= 1900 && ano <= 2100 && mes == 1 && dia > 0 && dia <= 31 ||
+                ano >= 1900 && ano <= 2100 && mes == 2 && dia > 0 && dia <= 28 ||
+                ano >= 1900 && ano <= 2100 && mes == 3 && dia > 0 && dia <= 31 ||
+                ano >= 1900 && ano <= 2100 && mes == 4 && dia > 0 && dia <= 30 ||
+                ano >= 1900 && ano <= 2100 && mes == 5 && dia > 0 && dia <= 31 ||
+                ano >= 1900 && ano <= 2100 && mes == 6 && dia > 0 && dia <= 30 ||
+                ano >= 1900 && ano <= 2100 && mes == 7 && dia > 0 && dia <= 31 ||
+                ano >= 1900 && ano <= 2100 && mes == 8 && dia > 0 && dia <= 31 ||
+                ano >= 1900 && ano <= 2100 && mes == 9 && dia > 0 && dia <= 30 ||
+                ano >= 1900 && ano <= 2100 && mes == 10 && dia > 0 && dia <= 31 ||
+                ano >= 1900 && ano <= 2100 && mes == 11 && dia > 0 && dia <= 30 ||
+                ano >= 1900 && ano <= 2100 && mes == 12 && dia > 0 && dia <= 31)
+                Console.WriteLine("A data informada é válida");
+            else Console.WriteLine("A data informada não é válida");
+        }
+    }
+}
---
+++ Lista 2/Exercício11/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma data no formato dd/mm/aaaa");
+            string[] data = Console.ReadLine().Split('/');
+            int dia = int.Parse(data[0]);
+            int mes = int.Parse(data[1]);
+            int ano = int.Parse(data[2]);
+            if (mes == 1)
+                Console.WriteLine($"A data é {dia} de janeiro de {ano}");
+            if (mes == 2)
+                Console.WriteLine($"A data é {dia} de fevereiro de {ano}");
+            if (mes == 3)
+                Console.WriteLine($"A data é {dia} de março de {ano}");
+            if (mes == 4)
+                Console.WriteLine($"A data é {dia} de abril de {ano}");
+            if (mes == 5)
+                Console.WriteLine($"A data é {dia} de maio de {ano}");
+            if (mes == 6)
+                Console.WriteLine($"A data é {dia} de junho de {ano}");
+            if (mes == 7)
+                Console.WriteLine($"A data é {dia} de julho de {ano}");
+            if (mes == 8)
+                Console.WriteLine($"A data é {dia} de agosto de {ano}");
+            if (mes == 9)
+                Console.WriteLine($"A data é {dia} de setembro de {ano}");
+            if (mes == 10)
+                Console.WriteLine($"A data é {dia} de outubro de {ano}");
+            if (mes == 11)
+                Console.WriteLine($"A data é {dia} de novembro de {ano}");
+            if (mes == 11)
+                Console.WriteLine($"A data é {dia} de dezembro de {ano}");
+        }
+    }
+}
